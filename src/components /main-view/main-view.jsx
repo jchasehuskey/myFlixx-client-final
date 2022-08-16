4c27f85ede2d350ea04ea80b6bf445182bcd487c
jchasehuskey <jchase.huskey@gmail.com>
2022-08-16 15:41:11 -0600

    updated movie.card.jsx and login-view.jsx
    included prop-types for movie-card and added onLoggedIn call for when a user successfully logs in

--- src/components /main-view/main-view.jsx
+++ src/components /main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { LoginView } from "../login-view/login-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -9,6 +10,7 @@ export class MainView extends React.Component {
     this.state = {
       movies: [],
       selectedMovie: null,
+      user: null,
     };
   }
 
@@ -30,13 +32,26 @@ export class MainView extends React.Component {
       selectedMovie: newSelectedMovie,
     });
   }
-  render() {
-    const { movies, selectedMovie } = this.state;
 
+  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+  onLoggedIn(user) {
+    this.setState({
+      user,
+    });
+  }
+  render() {
+    const { movies, selectedMovie, user } = this.state;
+
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user)
+      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+
+    // Before the movies have been loaded
     if (movies.length === 0) return <div className='main-view' />;
 
     return (
       <div className='main-view'>
+        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
         {selectedMovie ? (
           <MovieView
             movie={selectedMovie}
